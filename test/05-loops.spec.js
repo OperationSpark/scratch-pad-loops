10bc8f9ce09df3579ba100a92f552fca9ae5fb8c
Ben Spector <ben@operationspark.org>
2019-03-25 21:08:10 +0000

    consolidated testing

--- test/05-loops.spec.js
+++ test/05-loops.spec.js
@@ -21,8 +21,9 @@ describe('#loops', function() {
       expect(console.log.firstCall.calledWith(1)).to.be.true;
       expect(console.log.secondCall.calledWith(2)).to.be.true;
       expect(console.log.thirdCall.calledWith(3)).to.be.true;
-    });
-    it('should call log with the values in the Array', function() {
+      
+      console.log.reset();
+      
       assignment.printArrayValues(arrayTwo);
       expect(console.log.firstCall.calledWith('one')).to.be.true;
       expect(console.log.secondCall.calledWith('two')).to.be.true;
@@ -36,8 +37,9 @@ describe('#loops', function() {
       expect(console.log.firstCall.calledWith(3)).to.be.true;
       expect(console.log.secondCall.calledWith(2)).to.be.true;
       expect(console.log.thirdCall.calledWith(1)).to.be.true;
-    });
-    it('should call log with the values in the Array in reverse order', function() {
+      
+      console.log.reset();
+        
       assignment.printArrayValuesInReverse(arrayTwo);
       expect(console.log.firstCall.calledWith('four')).to.be.true;
       expect(console.log.secondCall.calledWith('three')).to.be.true;
@@ -48,8 +50,6 @@ describe('#loops', function() {
   describe('getObjectKeys()', function() {
     it('should return an Array containing the Object keys', function() {
       expect(assignment.getObjectKeys(object)).to.eql(['one', 'two', 'three']);
-    });
-    it('should return an Array containing the Object keys', function() {
       expect(assignment.getObjectKeys(objectTwo)).to.eql(['one', 'two', 'three', 'four']);
     });
   });
@@ -60,8 +60,9 @@ describe('#loops', function() {
       expect(console.log.firstCall.calledWith('one')).to.be.true;
       expect(console.log.secondCall.calledWith('two')).to.be.true;
       expect(console.log.thirdCall.calledWith('three')).to.be.true;
-    });
-    it('should call log with the keys in the Object', function() {
+      
+      console.log.reset();
+      
       assignment.printObjectKeys(objectTwo);
       expect(console.log.firstCall.calledWith('one')).to.be.true;
       expect(console.log.secondCall.calledWith('two')).to.be.true;
@@ -72,8 +73,6 @@ describe('#loops', function() {
   describe('getObjectValues()', function () {
         it('should return an Array containing the Object values', function () {
             expect(assignment.getObjectValues(object)).to.eql(['one', 'two', 'three']);
-        });
-        it('should return an Array containing the Object values', function () {
             expect(assignment.getObjectValues(objectTwo)).to.eql([1, 2, 3, 4]);
         });
     });
@@ -84,8 +83,9 @@ describe('#loops', function() {
       expect(console.log.firstCall.calledWith('one')).to.be.true;
       expect(console.log.secondCall.calledWith('two')).to.be.true;
       expect(console.log.thirdCall.calledWith('three')).to.be.true;
-    });
-    it('should call log with the values in the Object', function() {
+      
+      console.log.reset();
+      
       assignment.printObjectValues(objectTwo);
       expect(console.log.firstCall.calledWith(1)).to.be.true;
       expect(console.log.secondCall.calledWith(2)).to.be.true;
@@ -96,8 +96,7 @@ describe('#loops', function() {
   describe('getObjectLength()', function() {
     it('should return the number of key/value pairs in the Object', function() {
       expect(assignment.getObjectLength(object)).to.equal(3);
-    });
-    it('should return the number of key/value pairs in the Object', function() {
+    
       expect(assignment.getObjectLength(objectTwo)).to.equal(4);
     });
   });
@@ -108,8 +107,9 @@ describe('#loops', function() {
       expect(console.log.firstCall.calledWith('three')).to.be.true;
       expect(console.log.secondCall.calledWith('two')).to.be.true;
       expect(console.log.thirdCall.calledWith('one')).to.be.true;
-    });
-    it('should call log with the values in the Object in reverse order', function() {
+      
+      console.log.reset();
+    
       assignment.printObjectValuesInReverse(objectTwo);
       expect(console.log.firstCall.calledWith(4)).to.be.true;
       expect(console.log.secondCall.calledWith(3)).to.be.true;
